795163a4821d49868e7e486e04b0c720f91143c7
NichiforVlad <84733785+NichiforVlad@users.noreply.github.com>
2021-05-26 18:00:25 +0300

    Update CitireFisier.java

--- src/Services/CitireFisier.java
+++ src/Services/CitireFisier.java
@@ -28,7 +28,7 @@ public class CitireFisier {
         try {
             BufferedReader bufferedReader = new BufferedReader(
                             new FileReader(fileLocation));
-            //StringBuilder stringBuilder = new StringBuilder();
+
             String s;
             int index = 0;
             int[][] g = null;
@@ -73,11 +73,7 @@ public class CitireFisier {
             harta.setGraf(g);
             harta.setDistante(h);
             
-            //String[] x = stringBuilder.toString().split("#");
-//            harta.setOrase(numeOrase(x[0]));
-//            harta.setGraf(genereazaHarta(x[1]));
-//            harta.setDistante(genereazaDistante(x[2]));
-            
+
             
         } catch (Exception e) {
             e.printStackTrace();
@@ -86,35 +82,4 @@ public class CitireFisier {
         
     }
     
-//    private String[] numeOrase(String x) {
-//        String[] rezultat = x.split(" ");
-//        return rezultat;
-//    }
-//    
-//    private int[][] genereazaHarta(String x) {
-//        
-//        String[] randuri = x.split("\n");
-//        int[][] harta = new int[randuri.length][randuri.length];
-//        for (int i = 0; i < randuri.length; i++) {
-//            String[] temp = randuri[i].split("\t");
-//            
-//            for (int j = 0; j < temp.length; j++) {
-//                //harta[i][j] = Integer.parseInt(temp[j]);
-//                System.out.print(temp[j] + " ");
-//            }
-//            System.out.println();
-//        }
-//        
-//        return harta;
-//    }
-//    
-//    private HashMap<String, Integer> genereazaDistante(String x) {
-//        HashMap<String, Integer> hashMap = new HashMap<>();
-//        String[] randuri = x.split("\n");
-//        for (int i = 0; i < randuri.length; i++) {
-//            String[] rand = randuri[i].split(" ");
-//            hashMap.put(rand[0], Integer.parseInt(rand[1]));
-//        }
-//        return hashMap;
-//    }
 }
